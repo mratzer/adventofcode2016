8bbe23af1edbe6bd17738172a7372ee77910eb66
Markus Ratzer <mratzer85@gmail.com>
2016-12-03 09:56:44 +0100

    Day 3 - Solution part 2

--- src/main/java/org/marat/advent/day03/Day03.java
+++ src/main/java/org/marat/advent/day03/Day03.java
@@ -1,9 +1,9 @@
 package org.marat.advent.day03;
 
-import lombok.ToString;
 import org.apache.commons.lang3.StringUtils;
 import org.marat.advent.common.Util;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -11,35 +11,45 @@ import java.util.stream.Collectors;
 public class Day03 {
 
     public static void main(String[] args) {
-        Pattern delimiter = Pattern.compile("\n+");
+        Pattern delimiter = Pattern.compile("\\W+");
 
-        List<Triangle> triangles = Util.readElementsWithDelimiter("day03/input.txt", delimiter)
+        String[] sides = Util.readElementsWithDelimiter("day03/input.txt", delimiter)
                 .map(StringUtils::trim)
                 .filter(StringUtils::isNotEmpty)
-                .map(Triangle::new)
-                .filter(Triangle::isValid)
-                .collect(Collectors.toList());
+                .collect(Collectors.toList())
+                .toArray(new String[0]);
 
-        System.out.println(triangles.size());
+        if (sides.length % 3 != 0) {
+            throw new IllegalArgumentException();
+        }
+
+        List<Triangle> triangles = new ArrayList<>(sides.length / 3);
+
+        for (int i = 0; i < sides.length; i += 9) {
+            for (int j = 0; j < 3; j++) {
+                triangles.add(new Triangle(
+                        Integer.parseInt(sides[i + j]),
+                        Integer.parseInt(sides[i + j + 3]),
+                        Integer.parseInt(sides[i + j + 6])
+                ));
+            }
+        }
+
+        long count = triangles.stream().filter(Triangle::isValid).count();
+
+        System.out.println(count);
     }
 
-    @ToString
     static class Triangle {
 
         private final int a;
         private final int b;
         private final int c;
 
-        public Triangle(String input) {
-            String[] sides = StringUtils.split(input, ' ');
-
-            if (sides.length != 3) {
-                throw new IllegalArgumentException(String.format("Not a triangle, shape has %d sides.", sides.length));
-            }
-
-            a = Integer.parseInt(sides[0]);
-            b = Integer.parseInt(sides[1]);
-            c = Integer.parseInt(sides[2]);
+        public Triangle(int a, int b, int c) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
         }
 
         public boolean isValid() {
