42a49fdb66997a77928e0719f63da7430e0241e7
Markus Ratzer <mratzer85@gmail.com>
2016-12-03 09:46:16 +0100

    Day 3 - Solution part 1

--- src/main/java/org/marat/advent/day03/Day03.java
+++ src/main/java/org/marat/advent/day03/Day03.java
@@ -1,5 +1,6 @@
 package org.marat.advent.day03;
 
+import lombok.ToString;
 import org.apache.commons.lang3.StringUtils;
 import org.marat.advent.common.Util;
 
@@ -12,17 +13,39 @@ public class Day03 {
     public static void main(String[] args) {
         Pattern delimiter = Pattern.compile("\n+");
 
-        List<String> triangles = Util.readElementsWithDelimiter("day03/input.txt", delimiter)
+        List<Triangle> triangles = Util.readElementsWithDelimiter("day03/input.txt", delimiter)
                 .map(StringUtils::trim)
                 .filter(StringUtils::isNotEmpty)
-                .map(Day03::toTriangle)
+                .map(Triangle::new)
+                .filter(Triangle::isValid)
                 .collect(Collectors.toList());
 
-        System.out.println(triangles);
+        System.out.println(triangles.size());
     }
 
-    private static String toTriangle(String string) {
-        return string;
+    @ToString
+    static class Triangle {
+
+        private final int a;
+        private final int b;
+        private final int c;
+
+        public Triangle(String input) {
+            String[] sides = StringUtils.split(input, ' ');
+
+            if (sides.length != 3) {
+                throw new IllegalArgumentException(String.format("Not a triangle, shape has %d sides.", sides.length));
+            }
+
+            a = Integer.parseInt(sides[0]);
+            b = Integer.parseInt(sides[1]);
+            c = Integer.parseInt(sides[2]);
+        }
+
+        public boolean isValid() {
+            return (a + b) > c && (b + c) > a && (a + c) > b;
+        }
+
     }
 
 }
