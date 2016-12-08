2cfc8389b5db86d9d5c5bba9841ce5a52a5ad52b
Markus Ratzer <mratzer85@gmail.com>
2016-12-08 11:34:07 +0100

    Day 7 - Solution part 2

--- src/main/java/org/marat/advent/day07/Day07.java
+++ src/main/java/org/marat/advent/day07/Day07.java
@@ -8,13 +8,15 @@ import org.marat.advent.common.Util;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Day07 {
 
-    private static final Pattern STANDARD_PATTERN = Pattern.compile("^([a-z]+)(.*)");
+    private static final Pattern SUPERNET_PATTERN = Pattern.compile("^([a-z]+)(.*)");
     private static final Pattern HYPERNET_PATTERN = Pattern.compile("^\\[([a-z]+)](.*)");
 
     public static void main(String[] args) {
@@ -24,7 +26,7 @@ public class Day07 {
                 .map(StringUtils::trim)
                 .filter(StringUtils::isNotEmpty)
                 .map(Day07::parse)
-                .filter(Ipv7::supportsTls);
+                .filter(Ipv7::supportsSsl);
 
         System.out.println(ipv7Stream.count());
     }
@@ -34,19 +36,19 @@ public class Day07 {
     }
 
     public static Ipv7 parse(String addressPart, Ipv7 ipv7) {
-        Matcher goodMatcher = STANDARD_PATTERN.matcher(addressPart);
+        Matcher supernetMatcher = SUPERNET_PATTERN.matcher(addressPart);
 
-        if (goodMatcher.matches()) {
-            ipv7.addStandardSequence(goodMatcher.group(1));
+        if (supernetMatcher.matches()) {
+            ipv7.addSupernetSequence(supernetMatcher.group(1));
 
-            return parse(goodMatcher.group(2), ipv7);
+            return parse(supernetMatcher.group(2), ipv7);
         } else {
-            Matcher evilMatcher = HYPERNET_PATTERN.matcher(addressPart);
+            Matcher hypernetMatcher = HYPERNET_PATTERN.matcher(addressPart);
 
-            if (evilMatcher.matches()) {
-                ipv7.addHypernetSequence(evilMatcher.group(1));
+            if (hypernetMatcher.matches()) {
+                ipv7.addHypernetSequence(hypernetMatcher.group(1));
 
-                return parse(evilMatcher.group(2), ipv7);
+                return parse(hypernetMatcher.group(2), ipv7);
             } else {
                 return ipv7;
             }
@@ -59,16 +61,16 @@ public class Day07 {
 
         private final String complete;
 
-        private final List<String> standardSequences = new ArrayList<>();
+        private final List<String> supernetSequences = new ArrayList<>();
 
         private final List<String> hypernetSequences = new ArrayList<>();
 
-        public void addStandardSequence(String string) {
-            standardSequences.add(string);
+        public void addSupernetSequence(String string) {
+            supernetSequences.add(string);
         }
 
-        public List<String> getStandardSequences() {
-            return Collections.unmodifiableList(standardSequences);
+        public List<String> getSupernetSequences() {
+            return Collections.unmodifiableList(supernetSequences);
         }
 
         public void addHypernetSequence(String string) {
@@ -80,8 +82,28 @@ public class Day07 {
         }
 
         public boolean supportsTls() {
-            return standardSequences.stream().anyMatch(this::hasAbba)
+            return supernetSequences.stream().anyMatch(this::hasAbba)
                     && hypernetSequences.stream().noneMatch(this::hasAbba);
+        }
+
+        public boolean supportsSsl() {
+            Set<String> abas = supernetSequences.stream()
+                    .flatMap(sequence -> getAbasAndBabs(sequence).stream())
+                    .collect(Collectors.toSet());
+
+            Set<String> babs = hypernetSequences.stream()
+                    .flatMap(sequence -> getAbasAndBabs(sequence).stream())
+                    .collect(Collectors.toSet());
+
+            for (String aba : abas) {
+                for (String bab : babs) {
+                    if (isCorresponding(aba, bab)) {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
         }
 
         private boolean hasAbba(String string) {
@@ -104,6 +126,31 @@ public class Day07 {
             return chars[0] == chars[3] &&
                     chars[1] == chars[2] &&
                     chars[0] != chars[1];
+        }
+
+        private List<String> getAbasAndBabs(String string) {
+            List<String> abasAndBabs = new ArrayList<>();
+            char[] chars = new char[3];
+
+            for (int i = 0; i <= string.length() - 3; i++) {
+                string.getChars(i, i + 3, chars, 0);
+
+                if (isAbaOrBab(chars)) {
+                    abasAndBabs.add(new String(chars));
+                }
+            }
+
+            return abasAndBabs;
+        }
+
+        private boolean isAbaOrBab(char[] chars) {
+            assert chars.length == 3;
+
+            return chars[0] == chars[2] && chars[0] != chars[1];
+        }
+
+        private boolean isCorresponding(String aba, String bab) {
+            return aba.charAt(0) == bab.charAt(1) && aba.charAt(1) == bab.charAt(0);
         }
 
     }
